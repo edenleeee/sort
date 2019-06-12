9b9ead33faa482879778da4d746ff37eab44e378
Eden Le <nhockbake1@gmail.com>
2019-06-12 23:44:29 +0700

    add interpolationSearch

--- search.ts
+++ search.ts
@@ -1,10 +1,10 @@
 export class Search {
-  array:number[];
+  array: number[];
   constructor(array: number[]) {
     this.array = array;
   }
-  linearSearch(value:number) {
-    for ( let i = 0 ; i < this.array.length; i ++) {
+  linearSearch(value: number) {
+    for (let i = 0; i < this.array.length; i++) {
       if (this.array[i] === value) {
         return i;
       }
@@ -12,22 +12,54 @@ export class Search {
     return -1;
   }
 
-  binarySearch ( value: number ) {
-    let start = 0
-    let stop = this.array.length - 1
-    let middle = Math.floor((start + stop) / 2)
-  
+  binarySearch(value: number) {
+    let start = 0;
+    let stop = this.array.length - 1;
+    let middle = Math.floor((start + stop) / 2);
+
     while (this.array[middle] !== value && start < stop) {
       if (value < this.array[middle]) {
-        stop = middle - 1
+        stop = middle - 1;
       } else {
-        start = middle + 1
+        start = middle + 1;
       }
-  
-      middle = Math.floor((start + stop) / 2)
+
+      middle = Math.floor((start + stop) / 2);
     }
-    console.log(middle)
-    return (this.array[middle] !== value) ? -1 : middle
+    console.log(middle);
+    return this.array[middle] !== value ? -1 : middle;
   }
 
+  interpolationSearch( value: number) {
+    let leftIndex = 0;
+    let rightIndex = this.array.length - 1;
+  
+    while (leftIndex <= rightIndex) {
+      const rangeDelta = this.array[rightIndex] - this.array[leftIndex];
+      const indexDelta = rightIndex - leftIndex;
+      const valueDelta = value - this.array[leftIndex];
+  
+      if (valueDelta < 0) {
+        return -1;
+      }
+  
+      if (!rangeDelta) {
+        return this.array[leftIndex] === value ? leftIndex : -1;
+      }
+  
+      const middleIndex = leftIndex + Math.floor(valueDelta * indexDelta / rangeDelta);
+  
+      if (this.array[middleIndex] === value) {
+        return middleIndex;
+      }
+  
+      if (this.array[middleIndex] < value) {
+        leftIndex = middleIndex + 1;
+      } else {
+        rightIndex = middleIndex - 1;
+      }
+    }
+  
+    return -1;
+  }
 }
--- tests/search.ts
+++ tests/search.ts
@@ -1,5 +1,7 @@
+import { Sorter } from './../sort';
 import { expect } from 'chai';
 import { Search } from '../search';
+
 
 let tdd1 = [-2, -5, 78, 3, -10, 9, 15, 22];
 let tdd2 = [2, 5, 100, 500, 22, 77, 55, 332, 565, 11, 56, 99, 44, 43, 456];
@@ -8,15 +10,15 @@ let tdd3 = [45, 422, 33, 355, 6, 12, 7, 99, 5576, 2345, 5678, 34523452, 4567342,
 
 describe('Search', function () {
   it('Linear 1', function () {
-    let t1 = new Search(tdd1);
-    expect(t1.binarySearch(3)).to.equal(3);
+    let t1 = new Search(new Sorter(tdd1).bubbleSort());
+    expect(t1.interpolationSearch(3)).to.equal(3);
   });
   it('Linear 2', function () {
-    let t2 = new Search(tdd2);
-    expect(t2.binarySearch(332)).to.equal(7);
+    let t2 = new Search(new Sorter(tdd2).bubbleSort());
+    expect(t2.interpolationSearch(332)).to.equal(11);
   });
   it('Linear 3', function () {
-    let t3 = new Search(tdd3);
-    expect(t3.binarySearch(6)).to.equal(4);
+    let t3 = new Search(new Sorter(tdd3).bubbleSort());
+    expect(t3.interpolationSearch(6)).to.equal(0);
   });
 });
